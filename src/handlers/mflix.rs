9dc0cd11f5ee8e2b449d41c847eec530517d5842
transhapHigsn <fet.prashantsingh@gmail.com>
2022-12-10 02:29:53 +0530

    feat: add pagination, sorting query params for mflix sample dataset endpoint
    Signed-off-by: transhapHigsn <fet.prashantsingh@gmail.com>

--- src/handlers/mflix.rs
+++ src/handlers/mflix.rs
@@ -1,18 +1,67 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::{Query, State}, http::StatusCode, response::IntoResponse, Json};
 
 use futures::stream::StreamExt;
 use mongodb::{bson::doc, options::FindOptions, Client, Collection};
 
-use crate::structs::mflix::SampleUser;
+use crate::structs::mflix::{Pagination, Response, SampleUser};
 
-pub async fn sample_users(State(client): State<Client>) -> impl IntoResponse {
+
+pub async fn sample_users(State(client): State<Client>, pagination: Query<Pagination>) -> impl IntoResponse {
+    if pagination.page < 1 {
+        let response = Response {
+            success: false,
+            data: None,
+            error_message: Some("Page must be greater than or equal to 1.".into())
+        };
+        return (StatusCode::BAD_REQUEST, Json(response));
+    }
+
+    let sort_by: bson::Document;
+    let mut order: i64 = 1;
+    if let Some(val) = &pagination.sort_by {
+        if !(["_id", "name", "email"].contains(&val.as_str())) {
+            let response = Response {
+                success: false,
+                data: None,
+                error_message: Some("Invalid value passed for sort_by query parameter. Must be one of: _id, email or name.".into())
+            };
+            return (StatusCode::BAD_REQUEST, Json(response));
+        }
+
+        if let Some(ord) = &pagination.order {
+            if !(["asc", "desc"]).contains(&ord.as_str()) {
+                let response = Response {
+                    success: false,
+                    data: None,
+                    error_message: Some("Invalid value passed for order query parameter. Must be one of: asc or desc.".into())
+                };
+                return (StatusCode::BAD_REQUEST, Json(response));
+            }
+
+            if ord == "desc" {
+                order = -1;
+            }
+        };
+
+        sort_by = doc! {
+            val: order
+        };
+    } else {
+        sort_by = doc! {
+            "name": order
+        };
+    };
+
     let users_coll: Collection<SampleUser> = client
         .database("sample_mflix")
         .collection::<SampleUser>("users");
     let mut options = FindOptions::default();
-    options.limit = Some(10);
-    options.sort = Some(doc! {
-        "name": 1
+    options.limit = Some(pagination.per_page as i64);
+    options.skip = Some((pagination.page as u64 - 1) * pagination.per_page as u64);
+    options.sort = Some(sort_by);
+    options.projection = Some(doc! {
+        "name": 1,
+        "email": 1
     });
     let mut users_cursor = users_coll
         .find(None, options)
@@ -24,5 +73,11 @@ pub async fn sample_users(State(client): State<Client>) -> impl IntoResponse {
         users.push(doc.expect("could not load user info."));
     }
 
-    (StatusCode::OK, Json(users))
+    let response = Response {
+        success: true,
+        data: Some(users),
+        error_message: None
+    };
+
+    (StatusCode::OK, Json(response))
 }
--- src/structs/mflix.rs
+++ src/structs/mflix.rs
@@ -1,5 +1,6 @@
 use bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 
@@ -7,5 +8,23 @@ pub struct SampleUser {
     pub _id: ObjectId,
     pub name: String,
     pub email: String,
-    password: String,
+//    #[serde(skip_deserializing)]
+//    password: String,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Response {
+    pub success: bool,
+    pub data: Option<Vec<SampleUser>>,
+    pub error_message: Option<String>
+}
+
+#[skip_serializing_none]
+#[derive(Deserialize)]
+pub struct Pagination {
+    pub page: usize,
+    pub per_page: usize,
+    pub sort_by: Option<String>,
+    pub order: Option<String>
 }
